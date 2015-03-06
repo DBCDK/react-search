2c1a96dfc71f4913d21559e99a71e800bb1be3c6
Søren Vibjerg <vibjerg@gmail.com>
2015-03-06 09:58:54 +0100

    convert routes to es6

--- routes/routes.js
+++ routes/routes.js
@@ -1,27 +1,21 @@
 var express = require('express'),
-router = express.Router(),
-search = require('../lib/Search/Search.js'),
-template = require('../lib/Search/Transformers/WorkTransformer.js')
+    router = express.Router(),
+    search = require('../lib/Search/Search.js'),
+    template = require('../lib/Search/Transformers/WorkTransformer.js')
 
 
-/* GET home page. */
-router.get(['/search', '/search/*'], function(req, res) {
+// GET home page.
+router.get(['/search', '/search/*'], (req, res) => {
  res.render('search', {title: 'Main', module: 'search', state: ''});
 });
 
-/**
- * Search API routes
- */
-router.get('/API/search', function searchRoute(req, res) {
- var query = unescape(req.query.query);
- search.get(query)
- .transform(template)
- .then(function(result){
-  res.send(result);
- })
- .catch(function (err) {
-  res.send(err);
- });
+// Search API routes
+router.get('/API/search', (req, res) => {
+  let query = unescape(req.query.query);
+  search.get(query)
+  .transform(template)
+  .then((result) => res.send(result))
+  .catch((err) => res.send(err));
 });
 
  module.exports = router;
