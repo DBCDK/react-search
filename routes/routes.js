4ca55abd79ffaff30057b198ac9ee5cb5d8b9eb6
Søren Vibjerg <vibjerg@gmail.com>
2015-03-09 13:26:42 +0100

    Add default redirect to /search/

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,11 @@
 var express = require('express'),
     router = express.Router(),
     search = require('../lib/Search/Search.js'),
+    holdings = require('../lib/Holdingstatus/Holdingstatus.js'),
     template = require('../lib/Search/Transformers/dkabm.js')
 
 
-// GET home page.
+// GET search page.
 router.get(['/search', '/search/*'], (req, res) => {
  res.render('search', {title: 'Main', module: 'search', state: ''});
 });
@@ -14,8 +15,14 @@ router.get('/API/search', (req, res) => {
   let query = unescape(req.query.query);
   search.get(query)
   .transform(template)
+  //.add(holdings.get)
   .then((result) => res.send(result))
   .catch((err) => res.send(err));
 });
 
+router.get('*', (req, res) => res.redirect('/search/'));
+
+
  module.exports = router;
+
+
