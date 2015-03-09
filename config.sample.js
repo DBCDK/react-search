9ff5cc70e7872d1d0c2f7e1cfc26fceb656469b5
Søren Vibjerg <vibjerg@gmail.com>
2015-03-09 10:45:18 +0100

    update config docs

--- config.sample.js
+++ config.sample.js
@@ -1,7 +1,11 @@
 /**
- * @dbc configurations can be cloned from repo: ssh://git.dbc.dk/repos/dbc_config
+ * @dbc configurations can be fetched from git.dbc.dk:
+ *
+ * git clone -b develop ssh://git.dbc.dk/repos/dbc_config
  *
  * Make a symlink from react-search.config.js to <root>/config.js
+ *
+ * ln -s /<path>/<to>/<config>/react-search.config.js config.js
  */
 
 module.exports = {
