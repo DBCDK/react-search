564f379840b7c63aaf47cc76e6ae563e9cb5b676
Søren Vibjerg <vibjerg@gmail.com>
2015-03-09 10:29:06 +0100

    config sample

---
+++ config.sample.js
@@ -0,0 +1,16 @@
+/**
+ * @dbc configurations can be cloned from repo: ssh://git.dbc.dk/repos/dbc_config
+ *
+ * Make a symlink from react-search.config.js to <root>/config.js
+ */
+
+module.exports = {
+ opensearch : {
+  wsdl : /* url to wsdl */,
+  agency : /* agency */,
+  profile : /* profile */,
+ },
+ openholdingstatus : {
+  wsdl : /* url to wsdl */,
+ }
+}
