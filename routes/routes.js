97f195e6855fab29a40911b17c11827c25a74c09
Vibjerg <vibjerg@gmail.com>
2015-03-12 10:05:30 +0100

    Activate holdings for API search (this makes search slow)

--- routes/routes.js
+++ routes/routes.js
@@ -6,25 +6,28 @@ var express = require('express'),
 
 
 // GET search page.
-router.get(['/search', '/search/*'], (req, res) => {
-  res.render('search');
-});
+router.get(['/search', '/search/*'], (req, res) => res.render('search'));
 
 // Search API routes
 router.get('/API/search', (req, res) => {
   var query = unescape(req.query.query);
   // This is where the magic happens
-  //
+  try{
   //   1. Search performed
   search.get(query)
     // 2. Search object is transformed
     .transform(template)
     // 3. Holdings are added
-    //.add(holdings.get)
+    .add(holdings.get)
     // 4. Result is returned
     .then((result) => res.send(result))
     // Did any errors happen?
     .catch((err) => res.send(err));
+  }
+  catch (err) {
+      console.log(err);
+  };
+
 });
 
 // Fallback route : redirect non-targeted routes to /search/
