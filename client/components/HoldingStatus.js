f514eff8a03a67201971deda020826260466b741
Søren Vibjerg <vibjerg@gmail.com>
2015-03-13 14:15:20 +0100

    Holdings refaktored

--- client/components/HoldingStatus.js
+++ client/components/HoldingStatus.js
@@ -1,25 +1,8 @@
 var React = require('react');
 
-function parseHoldingsResponse(response) {
-  if (response.responder) {
-    return {
-      home: Date.parse(Date().slice(0, 15)) <= Date.parse(response.responder.expectedDelivery),
-      willLend: (response.responder.willLend === 'true'),
-      expectedDelivery: (new Date(response.responder.expectedDelivery)).toLocaleDateString(),
-    }
-  } else {
-      return {
-        home: false,
-        willLend: false,
-        error : true,
-        expectedDelivery: response.error.errorMessage,
-    }
-  }
-}
-
 var HoldingStatus = React.createClass({
   render: function() {
-    var holdings = parseHoldingsResponse(this.props.element);
+    var holdings = this.props.element;
     var cx = React.addons.classSet;
     var classes = cx({
       'home': holdings.home,
@@ -29,7 +12,7 @@ var HoldingStatus = React.createClass({
     });
 
     return (
-      <span className = {classes}>{holdings.expectedDelivery}< /span>
+      <span className = {classes}>{holdings.expectedDelivery || holdings.errorMessage}< /span>
     );
   }
 });
