6655d7041ca4d8064ff38e44af02daf1b5bccaf1
Vibjerg <vibjerg@gmail.com>
2015-03-15 22:13:42 +0100

    Documentation and cleanup of dispatcher

--- server/lib/dispatcher.js
+++ server/lib/dispatcher.js
@@ -1,10 +1,25 @@
 var socketIo = require('socket.io');
-var _socket;
+
+/**
+ * Contains all eventlisteners that should be instantiated on new connections
+ * @type {Array}
+ */
 var _listeners = new Array();
 
-function Dispatcer(server) {
+/**
+ * The Dispatcher is a wrapper for socket.io. It will handle all communication
+ * between server and client
+ *
+ * @param {[Object]} server a node http server is needed to initialize socket.io
+ */
+function Dispatcher(server) {
   socketIo(server).on('connection', makeConnection);
 
+  /**
+   * Callback method for new connections
+   * @param  {Socket} connection a new socket connection
+   * @return {null}
+   */
   function makeConnection(connection) {
     _listeners.map(listener => {
       connection.on(listener.type, (data) => {
@@ -13,6 +28,15 @@ function Dispatcer(server) {
     });
   }
 
+  /**
+   * Add new listener
+   *
+   * Listeners are added to an array of listeners that will be initiated on
+   * new connections
+   *
+   * @param {String}   type     Type of event to listen for
+   * @param {Function} callback Callback function on event
+   */
   function addListener(type, callback) {
     _listeners.push({
       type: type,
@@ -20,9 +44,11 @@ function Dispatcer(server) {
     });
   }
 
+  // Return factory, with a method for adding an event listener
   return {
     listen: addListener
   }
 }
 
-module.exports = Dispatcer;
+// Export Dispatcher module
+module.exports = Dispatcher;
