8525a8e376ead1ab5ce02960cdb2012cc1fcf0a6
Martin Møller <mmj@dbc.dk>
2015-03-18 13:10:50 +0100

    resolving merge conflict #KOP-7

--- server/lib/clients/OpenUserinfo.client.js
+++ server/lib/clients/OpenUserinfo.client.js
@@ -15,7 +15,7 @@ module.exports = {
   cart: {
     getCart: (user) => openUserinfo.request('getCart', {userId: user}, options, true),
     addCartContent: (content) => openUserinfo.request('addCartContent', content, options, true)
-  }
+  },
   user : {
     login : (user) => openUserinfo.request('login', {userId: user.name, userPinCode : user.password}, options)
   }
