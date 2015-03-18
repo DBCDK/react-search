b3e1e2f0e8ef5c2ea1ba7db0f5a961a7205e106b
Martin Møller <mmj@dbc.dk>
2015-03-18 09:21:38 +0100

    add method name in cart.transform (that probablky should change name to i.e. openuserinfo.transform) #KOP-7

--- server/modules/cart/cart.js
+++ server/modules/cart/cart.js
@@ -4,7 +4,7 @@ var Transform = require('./transform/cart.transform');
 module.exports = function(dispatcher) {
   dispatcher.listen('cartRequest', (data, connection) => {
     OpenUserInfo.cart(data)
-      .then(Transform)
+      .then(Transform.getCartTransform)
       .then((result) => connection.emit('cartResult', result));
   });
 };
--- server/modules/cart/transform/cart.transform.js
+++ server/modules/cart/transform/cart.transform.js
@@ -1,13 +1,15 @@
 var _ = require('lodash');
 
-module.exports = (response) => {
-  var inCartArr = [];
-  if(!_.isUndefined(response.cartContent)) {
-    for(var key in response.cartContent) {
-      var item = response.cartContent[key];
-      inCartArr.push(item.cartContentElement);
+module.exports = {
+  getCartTransform: (response) => {
+    var inCartArr = [];
+    if(!_.isUndefined(response.cartContent)) {
+      for(var key in response.cartContent) {
+        var item = response.cartContent[key];
+        inCartArr.push(item.cartContentElement);
+      }
     }
-  }
 
-  return inCartArr;
+    return inCartArr;
+  }
 };
