e1ecc078d5e9caaf9a83515bbbd242a338007bc8
Søren Vibjerg <vibjerg@gmail.com>
2015-03-19 10:12:23 +0100

    remove old authentication files

--- server/lib/authentication/io.auth.js
+++
@@ -1,9 +0,0 @@
-var passportSocketIo = require("passport.socketio");
-var cookieParser = require('cookie-parser')
-
-
-module.exports = function (io, sessionStore) {
-  io.use(function(socket, next){
-    sessionStore(socket.request, {}, next);
-  })
-}
