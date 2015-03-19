cbc5567a506f126689bca26f9e98afc1dc542efd
Martin Møller <mmj@dbc.dk>
2015-03-19 15:31:02 +0100

    syntax stuff

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ require("babel/register");
 
 // Import dependencies
 var express = require('express'),
-    io = require('socket.io'),
-    logger = require('morgan'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
-    path = require('path'),
-    exphbs  = require('express-handlebars'),
-    routes = require('./routes/routes'),
-    app = express(),
-    server = require('http').Server(app),
-    socket = io(server),
-    authentication = require('./services/modules/authentication/authentication'),
-    handlebars_helpers = require('./lib/handlebars/helpers');
-    modules = require('./services')(socket);
+  io = require('socket.io'),
+  logger = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  path = require('path'),
+  exphbs = require('express-handlebars'),
+  routes = require('./routes/routes'),
+  app = express(),
+  server = require('http').Server(app),
+  socket = io(server),
+  authentication = require('./services/modules/authentication/authentication'),
+  handlebars_helpers = require('./lib/handlebars/helpers'),
+  modules = require('./services')(socket);
 
 // Initialize authentication
 authentication.express(app);
@@ -26,8 +26,8 @@ app.set('port', process.env.PORT || 3000);
 
 // Setup view engine
 var hbs = exphbs.create({
-    defaultLayout: 'main',
-    helpers      : handlebars_helpers
+  defaultLayout: 'main',
+  helpers: handlebars_helpers
 });
 
 // Setup view engine
@@ -38,7 +38,7 @@ app.set('view engine', 'handlebars');
 // Setup helpers
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
 // Setup routing
@@ -49,37 +49,36 @@ app.use('/', routes);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 /// error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+if(app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
-
 // Startup server
-server.listen(app.get('port'), function () {
-    console.log('Server listening on ' + app.get('port'));
+server.listen(app.get('port'), function() {
+  console.log('Server listening on ' + app.get('port'));
 });
