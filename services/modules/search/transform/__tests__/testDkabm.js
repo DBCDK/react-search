432f42a7ff1cb69262c0b257810347b5e200852e
Martin Møller <mmj@dbc.dk>
2015-03-20 09:04:30 +0100

    debugging tests

--- services/modules/search/transform/__tests__/testDkabm.js
+++ services/modules/search/transform/__tests__/testDkabm.js
@@ -1,13 +1,14 @@
-'use strict'
+'use strict';
 
 jest.dontMock('../dkabm');
 
 describe('transform searchresult', function() {
-  var dkabm = require('../dkabm')
+  var dkabm = require('../dkabm');
   var obj = require('./data/searchresult.json');
   var expected = require('./data/transformed-object.json');
   it('transforms dkabm to dictated format', function() {
     var result = dkabm(obj);
+    console.log(result);
     expect(JSON.stringify(result.collections[1])).toEqual(JSON.stringify(expected));
   });
 });
--- services/modules/search/transform/dkabm.js
+++ services/modules/search/transform/dkabm.js
@@ -1,28 +1,28 @@
 var transform = require('jsonpath-object-transform');
 
 module.exports = function(result) {
-    var template = {
-        collections: ['$..collection.object', {
-            id: '$..identifier',
-            title: '$..record..title..$value',
-            type: ['$..record.type', {
-                value: '$..$value',
-                type: '$..xsi:type'
-            }],
-            subjects: ['$..record.subject.*', {
-                value: '$..$value',
-                type: '$..xsi:type'
-            }],
-            abstract: '$..record.abstract',
-            audience: ['$..record.audience..$value'],
-            publisher: ['$..record.publisher'],
-            contributor: ['$..record.contributor..$value'],
-            creator: ['$..record.creator..$value'],
-            date: '$..record.date',
-            format: '$..record.format',
-            language: '$..record.language..$value',
-        }],
-    };
-    var transformed = transform(result, template);
-    return transformed;
-}
+  var template = {
+    collections: ['$..collection.object', {
+      id: '$..identifier',
+      title: '$..record..title..$value',
+      type: ['$..record.type', {
+        value: '$..$value',
+        type: '$..xsi:type'
+      }],
+      subjects: ['$..record.subject.*', {
+        value: '$..$value',
+        type: '$..xsi:type'
+      }],
+      abstract: '$..record.abstract',
+      audience: ['$..record.audience..$value'],
+      publisher: ['$..record.publisher'],
+      contributor: ['$..record.contributor..$value'],
+      creator: ['$..record.creator..$value'],
+      date: '$..record.date',
+      format: '$..record.format',
+      language: '$..record.language..$value'
+    }]
+  };
+
+  return transform(result, template);
+};
